386d8978b3b88b52586d17f7a2c1e052ee9465d5
patrik200 <68776861+patrik200@users.noreply.github.com>
2021-12-01 20:43:52 +0300

    Add files via upload

--- python.py
+++ python.py
@@ -1,58 +1,223 @@
-clear = "\n" * 300
-print (clear)
-print('[0]выбор счёта\n[1]плотность')
-a = int(input('номер: '))
-print (clear)
-if a == 1:
-    print('p=m/v\nm=pv\nv=m/p')
-    print(clear)
-elif a == 0:
-    print('[1]плотность')
-    x = int(input('что нужно найти? '))
-    print(clear)
-    if x == 1:
-        ma = int(input("в чём масса?\n[1]в тоннах?\n[2]в килограммах?\n[3]в граммах? "))
-        print(clear)
-        va = int(input('в чём объём?\n[1]в метрах?\n[2]в литрах?\n[3]в сантиметрах? '))
-        print(clear)
-        ma1 = int(input('а в чём нада масса?\n[1]в килограммах?\n[2]граммах? '))
-        print(clear)
-        ma2 = int(input('кол-во массы: '))
-        print(clear)
-        va2 = int(input('кол-во объёма '))
+print("Кароч етата функция вывода и переменная плюс разделитель")
+print("потом соединение сток (хз нахуя, но оно есть)")
+print("\\n <--- переводит на новую строку (долго пытался понять, где ошибка)")
+print("\\t это вывод пробела как через таб")
+print("выводить любой символ без изменений надо ставить перед ним \\")
+
+print('x = "А Б О Б А"')
+
+print("'Здрасти', x, sep='\\t|\\t', end=\"\t|\t\")")
+
+print('Ты \"пидр\"', end='\n\n')
+
+print("СЛЫШЕШЬ ТЫ \n\nп\n\nи\n\nд\n\nо\n\nр\n\nа\n\nс")
+
+print("есть ещё и функции min и max, они показывают мин. значение и макс.")
+print("abc <--- было -5 стало 5")
+print("pow это степень (ну или **)")
+print("round округление")
+
+print ("txt = input(\"Какой у тебя рост? \n\")")
+print ("print(\"Твой рост:\", txt)")
+
+print("del txt")
+
+print ("int1 = int(input('Введи  свой рост: '))")
+print ("int2 = int(input('Введи  рост друга: '))")
+print("print('Ваша разница:', int1 - int2)")
+
+print("data = input()")
+
+print ("number = 6 if data == 'five' else 0")
+
+print ("print(number)")
 
 
-        if ma1 == 1:
-            va1 = 1;
-        elif ma1 == 2:
-            va1 = 3;
+
+
+print("---------------------------------------------------------------------------------------------------------------------")
 
 
 
-        if ma == 1 and ma1 == 1:
-            ma3 = ma2 * 1000
-        elif ma == 1 and ma1 == 2:
-            ma3 = ma2 * 1000*1000
-        elif ma == 2 and ma1 == 1:
-            ma3 = ma2
-        elif ma == 2 and ma1 == 2:
-            ma3 = ma2 * 1000
-        elif ma == 3 and ma1 == 1:
-            ma3 = ma2/1000
-        elif ma == 3 and ma1 == 2:
-            ma3 = ma2
-        if va == 1 and va1 == 1:
-            va3 = va2
-        elif va == 1 and va1 == 2:
-            va3 = va2 * 1000
-        elif va == 1 and va1 == 3:
-            va3 = va2 * 1000000
-        elif va == 2 and va1 == 1:
-            va3 = va2/1000
-        elif va == 1 and va1 == 2:
-            va3 = va2
-        elif va == 1 and va1 == 2:
-            va3 = va2 * 1000
-        if ma1 == 1:
-            p = ma3/va3
-            print(p, "кг/м^3")
+
+print ("for i in range(6)")
+print ("print(i)")
+
+print ("txt = \"Ты пидор 2.0\"")
+print ("for i in txt:")
+print("(i)")
+
+print("txt = input('Вводи: ')")
+print("count = 0")
+print("for i in txt:\n"
+"if i == '!':\n"
+"       count += 1\n"
+"print(count)\n\n"
+
+"print('САМАЯ НУЖНАЯ ПРОГРАММА В МИРЕ'\n)"
+"i = int(input(\"Введите число: \"))\n"
+"if i == 1000:\n"
+"   while i > 4:\n"
+       "print(i)\n"
+       "i -= 7\n"
+"print(\"Я гуль\")\n"
+"i = 5\n"
+"while i >= -5:\n"
+   "print(i)\n"
+   "i -= 2\n\n"
+
+"f = None\n"
+"for i in ""Здрасти:\n"
+"   if i == 'с':\n"
+"       f = True\n"
+"       break\n"
+"   else:\n"
+"       f = False\n"
+"if f == True:\n"
+"   print(\"ты педафил!\")\n"
+"else:\n"
+"   print(\"живи гад\")\n\n"
+
+"num = [7, 10, 3.1, True, \"гандон\"]\n"
+"num1 = [93, 2, \"dfsdf\", [4, 3.14, 'хахол']]\n\n"
+
+"print(num[4])\n"
+"print(num1[-1][1]\n\n)"
+
+"lol = [5, 2, 7]\n\n"
+
+"print(lol, end='\\n\\n')\n\n"
+
+"lol.append(100) #добавление элемента\n\n"
+
+"print(lol, end='\\n\\n')\n\n"
+
+"lol.insert(1,'хахол') #замена по индексу (было 2, стало... то что стало)\n\n"
+
+"print(lol, end='\\n\\n')\n\n"
+
+"lol.extend([2, 3, 4, False]) #добавление списком\n"
+"b = [1, 120]\n"
+"lol.extend(b)#или так\n\n"
+
+"print(lol, end='\\n\\n')\n\n"
+
+"lol.reverse() #места наоборот\n\n"
+
+"print(lol, end='\\n\\n')\n\n"
+
+"lol.pop(1) #удаление из списка по номеру или без\n\n"
+
+"print(lol, end='\\n\\n')\n\n"
+
+"lol.remove(120) #удаление по названию\n\n"
+
+"print(lol, end='\\n\\n')\n\n"
+
+"lol_count = lol.count(2) #сколько таких же элементов в списке, тобишь то что в (), то и ищет\n\n"
+
+"print(\"ета стока \"2\" в списке lol:\", lol_count, end='\\n\\n')\n\n"
+
+"print(\"Длинна етого списка:\", len(lol),\"элементов\" , end='\\n\\n') #длинна списка\n\n"
+
+"for i in lol:\n"
+"    print(i, end=\"\\t|\\t\")\n\n"
+
+"print(\"\\n\")\n"
+"lol.clear()# НАХУЙ ВСЁ\n\n"
+
+"print(\"нихуя нету в списке\", lol, end='\\n\\n')\n\n"
+
+"x = int(input(\"Введи хоть что-то лентяй ванючий:\t\"))\n"
+"user_list = []\n"
+"i = 0\n\n"
+
+"for i in range(x):\n"
+"    txt = \"Введи элемент №\" + str(i + 1) + \": \"\n"
+"    user_list.append(input(txt))\n"
+"    i+=1\n"
+"del i\n"
+"for i in user_list:\n"
+"    print(i, end='\t|\t')\n\n"
+
+"main = 5, 4, 10, True\n\n"
+
+"main_next = []\n\n"
+
+"i = []\n\n"
+
+"for i in main:\n"
+"    main_next.append(i)\n"
+"print(main_next)\n\n"
+
+"count = {'laung': 'Russian', 'population': 144000}\n\n"
+
+"for key, el in count.items():   # .items разбивает ключи с элементами на картежи\n"
+"    print(key, '\\t-\\t', el, end='\\n\\n')\n\n"
+
+"print(count['laung'], end='\\n\\n')\n\n"
+
+"print(count.get('laung'), end='\\n\\n')   #тоже самое, что и через кв.скобки\n\n"
+
+"count.popitem()     #удаление последнего элемента\n\n"
+
+"count.pop('laung')  #удаление по ключу\n\n"
+
+"print(count, end='\\n\\n')\n\n"
+
+"print(count.keys(), end='\\n\\n')     #выводит только ключи\n\n"
+
+"count['laung'] = 'De'   # замена элемта\n\n"
+
+"print(count.values(), end='\n\n')   #выводит элементы\n\n"
+
+"count.clear()#НАХУЙ ВСЁ ПОД None ХВХАХАХХВХВХАХВАХАХВАХВХАХВАХВАХ\n\n"
+
+"person = {\n"
+"    'user_1':{\n"
+"        'adres': ['Смола', 'ул. Пушкина'],\n"
+"        'Fam': 'Гнидов'\n"
+"    },\n"
+"    'user_2':{\n"
+
+"    }\n"
+"}\n"
+"print(person['user_1'])\n\n"
+
+"def minimum(insert):\n"
+"    min_num = insert[0]\n"
+"    for el in insert:\n"
+"        if el < min_num:\n"
+"            min_num = el\n"
+"    print(min_num)\n"
+"list_test = [2.1, 13, 25, 1]\n\n"
+
+"minimum(list_test)\n\n"
+
+"data = input('Я хочу узнать тебя по лучше ,_,\\t\\t')\n\n"
+
+"file = open('lol.txt', 'w')\n\n"
+
+"if data != None:\n"
+"    print('Спасиба за рассказ <З')\n"
+"else:\n"
+"    print('Ну спасибо блин ')\n\n"
+
+"file.write(data)\n\n"
+
+"file.close()\n\n"
+
+"x = 0\n"
+"while x==0:\n"
+"    try:\n"
+"        x = int(input('Введи число БЫДЛО:'))\n"
+"        print(x)\n"
+"    except ValueError:\n"
+"        print('Какая-то  хуйня')\n"
+"print(dt.datetime.now().time())\n\n"
+
+"print(platform.system())\n\n"
+
+"from math import sqrt as s, ceil\n\n"
+
+"print(ceil(s(3.14))))")
